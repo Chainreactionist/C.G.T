c5dac70a57af9fbc09a783e158c4d3101b1ed80b
Chainreactionist <Ehinobs@gmail.com>
2022-04-10 12:27:59 -0400

    Updated Template

--- src/server/Template.lua
+++ src/server/Template.lua
@@ -1,37 +1,37 @@
 --!strict
 
 --[[
-	DESCRIPTION:
+{C.G.T}
+
+-[ModuleName]---------------------------------------
+	Module description
+	
+	Members:
+	
+	Functions:
+	
+	Members [ClassName]:
+	
+	Methods [ClassName]:
+	
 --]]
 
---[[
-	MEMBERS:
+local SETTINGS = {}
 
-	FUNCTIONS:
+----- Loaded Modules -----
 
-	MEMBERS [ClassName]:
+----- Module Table -----
 
-	METHODS [ClassName]:
+local Module = {}
 
-	LINKS:
-]]
+----- Private Variables -----
 
------------------>> SETTINGS
+----- Private functions -----
 
------------------>> SERVICES
+----- Public -----
 
------------------>> LOADED MODULES
+----- Initialize -----
 
------------------>> MODULE
+----- Connections -----
 
------------------>> PRIVATE VARIABLES
-
------------------>> PRIVATE FUNCTIONS
-
------------------>> PUBLIC VARIABLES
-
------------------>> PUBLIC FUNCTIONS
-
------------------>> INITIALIZE & CONNECTIONS
-
------------------>> RETURN
+return Module
